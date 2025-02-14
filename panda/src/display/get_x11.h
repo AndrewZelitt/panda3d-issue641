61e67ce5cf2dea95e0e795491579d2d85c7117e9
AZelitt <azelitt@ufl.edu>
2025-02-14 17:17:30 -0500

    Added functionality for disabling screen saver when using X11 and when using Windows. For X11 it will require the inclusion of the package "libxss-dev" into the required packages.

--- panda/src/display/get_x11.h
+++ panda/src/display/get_x11.h
@@ -11,48 +11,50 @@
  * @date 2011-08-28
  */
 
-#ifndef GET_X11_H
-#define GET_X11_H
-
-#include "pandabase.h"
-
-#ifdef HAVE_X11
-// This header file is designed to help work around some of the namespace
-// spamming that X11 causes, by renaming the symbols that X11 declares that
-// are known to conflict with other library names (like Apple's Core Graphics,
-// for instance).
-
-// In order for this to work, everyone who uses X11 within Panda should
-// include this file instead of including the X11 headers directly.
-
-#ifdef CPPPARSER
-// A simple hack so interrogate can get all of the necessary typenames.
-typedef struct _XDisplay X11_Display;
-typedef unsigned int XID;
-typedef unsigned int Atom;
-typedef unsigned int Cardinal;
-typedef XID Colormap;
-typedef XID X11_Window;
-typedef XID X11_Cursor;
-typedef struct _XIM *XIM;
-typedef struct _XIC *XIC;
-struct XErrorEvent;
-struct XVisualInfo;
-#define Bool int
-#define Status int
-#define True 1
-#define False 0
-#else
-
-#include "pre_x11_include.h"
-#include <X11/XKBlib.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/keysym.h>
-#include <X11/Xatom.h>
-#include "post_x11_include.h"
-
-#endif  // CPPPARSER
-#endif  // HAVE_X11
-
-#endif
+ #ifndef GET_X11_H
+ #define GET_X11_H
+ 
+ #include "pandabase.h"
+ 
+ #ifdef HAVE_X11
+ // This header file is designed to help work around some of the namespace
+ // spamming that X11 causes, by renaming the symbols that X11 declares that
+ // are known to conflict with other library names (like Apple's Core Graphics,
+ // for instance).
+ 
+ // In order for this to work, everyone who uses X11 within Panda should
+ // include this file instead of including the X11 headers directly.
+ 
+ #ifdef CPPPARSER
+ // A simple hack so interrogate can get all of the necessary typenames.
+ typedef struct _XDisplay X11_Display;
+ typedef unsigned int XID;
+ typedef unsigned int Atom;
+ typedef unsigned int Cardinal;
+ typedef XID Colormap;
+ typedef XID X11_Window;
+ typedef XID X11_Cursor;
+ typedef struct _XIM *XIM;
+ typedef struct _XIC *XIC;
+ struct XErrorEvent;
+ struct XVisualInfo;
+ #define Bool int
+ #define Status int
+ #define True 1
+ #define False 0
+ #else
+ 
+ #include "pre_x11_include.h"
+ #include <X11/XKBlib.h>
+ #include <X11/Xlib.h>
+ #include <X11/Xutil.h>
+ #include <X11/keysym.h>
+ #include <X11/Xatom.h>
+ #include <X11/extensions/scrnsaver.h>
+ #include "post_x11_include.h"
+ 
+ #endif  // CPPPARSER
+ #endif  // HAVE_X11
+ 
+ #endif
+ 
